2e2aa6b62d5234bb6d4b5286a43552b4dd45be3e
cghidalgos <cgiohidalgo@gmail.com>
2026-02-20 19:55:28 -0500

    sofi, ahora abre en localhost:3000/padia/

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/padia/",
   server: {
     host: "::",
     port: 8080,
